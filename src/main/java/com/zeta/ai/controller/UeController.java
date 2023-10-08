1952bb97d9d6e0581e3a6043783278580eba820e
Louis <qingyanjiu@hotmail.com>
2023-10-08 09:30:02 +0800

    增加心跳检测接口

--- src/main/java/com/zeta/ai/controller/UeController.java
+++ src/main/java/com/zeta/ai/controller/UeController.java
@@ -1,19 +1,37 @@
 package com.zeta.ai.controller;
 
 import com.zeta.ai.utils.WebSocketUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.zeta.ai.utils.WebSocketUtil.ONLINE_SESSION;
 
+@Slf4j
 @RestController
 @RequestMapping("/socket")
 public class UeController {
 
     @Autowired
     private WebSocketController webSocketController;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @GetMapping("/keepalive")
+    public ResponseEntity keepalive(String test) {
+        Map map = new HashMap();
+        map.put("success", true);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
 
     @RequestMapping("/sendMsg")
     public ResponseEntity sendMsg(String userNo, String message) {
